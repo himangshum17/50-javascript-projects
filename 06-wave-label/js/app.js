8556dc16834db0f60f4c43a5611ee33bd53d2d8f
Himangshu Mondal <mailtoherock@gmail.com>
2021-06-03 21:44:32 +0530

    wave label javascript functionality added and html and css updated

---
+++ 06-wave-label/js/app.js
@@ -0,0 +1,18 @@
+function wavyLabels() {
+  const labels = document.querySelectorAll('label');
+
+  labels.forEach(function (label) {
+    let labelText = label.textContent;
+    let labelTextArr = labelText.split('');
+    label.textContent = '';
+    labelTextArr
+      .map((letter, i) => {
+        let spanEl = document.createElement('span');
+        spanEl.style.transitionDelay = `${i * 48}ms`;
+        spanEl.textContent = `${letter}`;
+        label.append(spanEl);
+      })
+      .join('');
+  });
+}
+wavyLabels();
